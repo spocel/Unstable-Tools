696e5ef71b928b10c2670cc1fa76989d86d00acf
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-07-29 19:11:39 -0400

    fix color issue

--- src/main/java/com/tfar/unstabletools/Client.java
+++ src/main/java/com/tfar/unstabletools/Client.java
@@ -1,15 +1,16 @@
 package com.tfar.unstabletools;
 
-import net.minecraft.client.renderer.color.ItemColors;
-import net.minecraft.item.Item;
+import com.tfar.unstabletools.item.IItemColored;
 import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
+@Mod.EventBusSubscriber(value = Side.CLIENT)
 public class Client {
   @SubscribeEvent
-  public static void colors(ColorHandlerEvent.Item e){
-    ItemColors itemColors = e.getItemColors();
-    itemColors.registerItemColorHandler(ObjectHolders.unstableIngot::getColor,(Item)ObjectHolders.unstableIngot);
-    itemColors.registerItemColorHandler(ObjectHolders.divisionSign::getColor,(Item)ObjectHolders.divisionSign);
+  public static void colors(ColorHandlerEvent.Item e) {
+    UnstableTools.MOD_ITEMS.stream().filter(item -> item instanceof IItemColored).forEach(item -> e.getItemColors().registerItemColorHandler(((IItemColored) item)::getColor, item));
   }
 }
+
